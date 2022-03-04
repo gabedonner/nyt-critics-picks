4f12d54e371acffa81a8424fd0554c74a7e2f58e
gdonner <gtdonner1@gmail.com>
2022-03-03 23:12:11 -0500

    fancy buttons

--- components/counter.js
+++ components/counter.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import styles from '../styles/Home.module.css'
-
+import Button from '@mui/material/Button';
 
 const blah = 'blahblahblah'
 
@@ -13,12 +13,30 @@ const Counter = () => {
         <div className={styles.container}>
             <h2>Check out this counter</h2>
             <h4>The count is {count}</h4>
-            <button onClick={() => setCount(count + 1)}>
+            {/*<button onClick={() => setCount(count + 1)}>
                 Add
             </button>
             <button onClick={() => setCount(count - 1)}>
                 Subtract
-            </button>
+            </button> */}
+            <Button 
+                onClick={() => setCount(count + 1)} 
+                variant="outlined"
+                sx = {{
+                    mx: 2,
+                }}
+                >
+                Add
+            </Button>
+            <Button 
+                onClick={() => setCount(count - 1)} 
+                variant="outlined"
+                sx = {{
+                    mx: 2,
+                }}
+                >
+                Subtract
+            </Button>
             {/* <p>{blah}</p> */}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          This is{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
