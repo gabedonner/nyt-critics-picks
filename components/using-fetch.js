bc857dfb2e1b774245b4dcc02928178a5edb0d96
gdonner <gtdonner1@gmail.com>
2022-03-23 14:44:11 -0400

    testing built-in fetch api

---
+++ components/using-fetch.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react'
+import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button'
+
+///reviews/search.json?query=big&opening-date=1980-01-01:1990-01-01
+//API KEY joZEGXraTg5TwCzoVbILHCL6F0e2C9vG
+
+//https://www.codingdeft.com/posts/react-fetch-data-api/#fetching-data-in-react-using-async-await
+
+
+const UsingFetch = () => {
+  const [users, setUsers] = useState([])
+
+  const fetchData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default UsingFetch
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Counter from '../components/counter.js'
 import {useEffect} from 'react'
+import UsingFetch from '../components/using-fetch.js'
 
 ///reviews/search.json?query=big&opening-date=1980-01-01:1990-01-01
 //API KEY joZEGXraTg5TwCzoVbILHCL6F0e2C9vG
@@ -37,44 +38,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hi
+          NYT Movie Picks
         </h1>
 
         <p className={styles.description}>
           This is{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+        
+        <UsingFetch />
 
-
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
       </main>
 
       <footer className={styles.footer}>
