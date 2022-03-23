7426e7c13873985838c4bf9209562cca8c8a1256
gdonner <gtdonner1@gmail.com>
2022-03-23 19:39:37 -0400

    mapping over api search results to make ul

--- components/using-fetch.js
+++ components/using-fetch.js
@@ -1,14 +1,45 @@
 import React, {useEffect, useState} from 'react'
 import styles from '../styles/Home.module.css'
-import Button from '@mui/material/Button'
 
 ///reviews/search.json?query=big&opening-date=1980-01-01:1990-01-01
 //API KEY joZEGXraTg5TwCzoVbILHCL6F0e2C9vG
 
 //https://www.codingdeft.com/posts/react-fetch-data-api/#fetching-data-in-react-using-async-await
 
+const url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=joZEGXraTg5TwCzoVbILHCL6F0e2C9vG"
 
 const UsingFetch = () => {
+  const [movies, setMovies] = useState([])
+
+  const fetchData = () => {
+    fetch(url)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setMovies(data.results)
+        console.log(data.results)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const movieList = movies.map((movieItem, index) => <li key={index}>{movieItem.display_title}</li>);
+
+  return (
+    <div>
+      <ul>{movieList}</ul>
+      <p>{JSON.stringify(movies)}</p>
+    </div>
+  )
+}
+
+export default UsingFetch
+
+
+/* const UsingFetch = () => {
   const [users, setUsers] = useState([])
 
   const fetchData = () => {
@@ -38,4 +69,4 @@ const UsingFetch = () => {
   )
 }
 
-export default UsingFetch
+export default UsingFetch */
