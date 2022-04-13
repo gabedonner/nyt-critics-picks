9acf9364969392d721b02b12dd05ac5f8f6b8ddb
gdonner <gtdonner1@gmail.com>
2022-04-12 21:32:28 -0400

    remove footer for now

--- pages/index.js
+++ pages/index.js
@@ -44,8 +44,8 @@ export default function Home() {
 
       </main>
 
-      <footer className={styles.footer}>
-        {/* <a
+      {/* <footer className={styles.footer}>
+        <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -54,9 +54,9 @@ export default function Home() {
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a> */}
+        </a>
         <p>Footer</p>
-      </footer>
+      </footer> */}
     </div>
   )
 }
