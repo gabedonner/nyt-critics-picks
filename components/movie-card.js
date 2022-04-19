38f96c6284758f652c7b0fb6c4ab4ecf8abf0751
gdonner <gtdonner1@gmail.com>
2022-04-18 21:04:05 -0400

    make dummy movieCard component

---
+++ components/movie-card.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button';
+import Image from 'next/image'
+import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
+
+
+const MovieCard = () => {
+
+    console.log("title")
+
+    return (
+        <div>hi</div>
+    )
+}
+
+export default MovieCard 
