d6e787d8025d010429fb8663242aa5c424bfcb85
gdonner <gtdonner1@gmail.com>
2022-04-26 19:59:13 -0400

    change naming to match rest of app

--- components/infinite-scroll.js
+++ components/infinite-scroll.js
@@ -3,7 +3,7 @@ import { useRef, useCallback } from "react";
 //from:
 //https://medium.com/swlh/a-comprehensive-guide-to-load-more-button-and-infinite-scrolling-in-react-js-bd88edf74d5a
 
-const useInfiniteScroll = (callback, isFetching) => {
+const useInfiniteScroll = (callback, loading) => {
   //here we use useRef to store a DOM node and the returned object will persist regardless of re-renders
   const observer = useRef();
 
@@ -11,7 +11,7 @@ const useInfiniteScroll = (callback, isFetching) => {
   //which is guaranteed to have the same reference
   const lastElementRef = useCallback(
     (node) => {
-      if (isFetching) return;
+      if (loading) return;
 
       //stop watching targets, you can think of it as a reset
       if (observer.current) observer.current.disconnect();
@@ -26,7 +26,8 @@ const useInfiniteScroll = (callback, isFetching) => {
       //if there is a node, let the intersection observer watch that node
       if (node) observer.current.observe(node);
     },
-    [callback, isFetching]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [callback, loading]
   );
 
   //return reference to the last element
