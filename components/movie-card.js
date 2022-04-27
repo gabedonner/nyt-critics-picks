3d0f8200a5375e7341247cc16d3e27676e2d6b66
gdonner <gtdonner1@gmail.com>
2022-04-27 12:31:12 -0400

    add motion to moviecard when entering view

--- components/movie-card.js
+++ components/movie-card.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Image from 'next/image'
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import { useInView } from 'react-intersection-observer'
+import { motion, useAnimation } from 'framer-motion'
+
 
 
 
 const MovieCard = ({movieData}) => {
+    const controls = useAnimation();
+    const [ref, inView] = useInView();
+    const movieAnimations = {
+        hidden: { opacity: 0, y: 5 },
+        visible: { opacity: 1, y: 0 },
+        tap: { scale: 0.9 },
+    }
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+    }, [controls, inView]);
 
     return (
         <Link href={movieData.link.url} sx={{
@@ -20,10 +35,10 @@ const MovieCard = ({movieData}) => {
             transition: 'all .5s',
             transitionTimingFunction: 'cubic-bezier(0.6, 0.01, -0.05, 0.9)',
             '&:hover': {
-              backgroundColor: "white",
-              boxShadow: "rgba(0, 0, 0, 0.55) 0px 30px 90px;", 
-              // transform: 'scale(1.01)'
-              //transform: 'translate(0, -2px);'
+            backgroundColor: "white",
+            boxShadow: "rgba(0, 0, 0, 0.55) 0px 30px 90px;", 
+            // transform: 'scale(1.01)'
+            //transform: 'translate(0, -2px);'
 
             }}} 
             //underline="hover" 
@@ -31,16 +46,24 @@ const MovieCard = ({movieData}) => {
             color="inherit" 
             target="_blank" rel="noopener noreferrer">
 
-            <Image src={movieData.multimedia === null ? "/vercel.svg" : movieData.multimedia.src} alt={movieData.display_title} width={600} height={400} sx={{imageRendering: 'crisp-edges'}} />
-            <Box sx={{px: 2, pb: 2}}>
-              <h3>{movieData.display_title === "" ? "[No title found]" : movieData.display_title }</h3>
-              <i>{movieData.summary_short}</i>
-              <p>{"- " + movieData.byline}</p>
-              {/* <p>{movie.publication_date}</p> */}
-              {/* <p>{index + 1}</p> */}
-              {/* <a href={movie.link.url}>link to article &#8594;</a> */}
-            </Box>
-          </Link>
+            <motion.div
+            variants={movieAnimations}
+            ref={ref}
+            initial="hidden"
+            animate={controls}
+            whileTap="tap">
+                <Image src={movieData.multimedia === null ? "/vercel.svg" : movieData.multimedia.src} alt={movieData.display_title} width={600} height={400} sx={{imageRendering: 'crisp-edges'}} />
+                <Box sx={{px: 2, pb: 2}}>
+                <h3>{movieData.display_title === "" ? "[No title found]" : movieData.display_title }</h3>
+                <i>{movieData.summary_short}</i>
+                <p>{"- " + movieData.byline}</p>
+                {/* <p>{movie.publication_date}</p> */}
+                {/* <p>{index + 1}</p> */}
+                {/* <a href={movie.link.url}>link to article &#8594;</a> */}
+                </Box>
+            </motion.div>
+
+        </Link>
     )
 }
 
