fc2cbf44b1f714767cf6bb6694e3664c3479a919
gdonner <gtdonner1@gmail.com>
2022-05-02 15:45:48 -0400

    remove window ref

--- components/app-bar.js
+++ components/app-bar.js
@@ -57,12 +57,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 function HideOnScroll(props) {
   const { children, window } = props
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-  })
+  const trigger = useScrollTrigger()
 
   return (
     <Slide appear={false} direction="down" in={!trigger}>
