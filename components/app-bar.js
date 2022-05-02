97195a7dd7429b8957558d3100e1075bf6ce1103
gdonner <gtdonner1@gmail.com>
2022-05-02 19:42:38 -0400

    comment out extra styles

--- components/app-bar.js
+++ components/app-bar.js
@@ -28,15 +28,15 @@ const Search = styled('div')(({ theme }) => ({
   },
 }))
 
-const SearchIconWrapperButton = styled(IconButton)(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}))
+// const SearchIconWrapperButton = styled(IconButton)(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: '100%',
+//   position: 'absolute',
+//   pointerEvents: 'none',
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+// }))
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
