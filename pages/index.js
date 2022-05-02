14db3e0b79db80392ec8d472ded0f61806977a86
gdonner <gtdonner1@gmail.com>
2022-05-02 05:41:14 -0400

    stage hide-able navbar

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,25 @@ import ScrollToTop from '../components/scroll-to-top.js'
 import AppBar from '../components/app-bar.js'
 import { styled } from '@mui/material/styles'
 import Container from '@mui/material/Container'
+import AppBarHide from '../components/hide-on-scroll-bar.js'
+
+//lifting state:
+//https://www.youtube.com/watch?v=rdwc4JmX_fU
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar)
 
 export default function Home() {
   return (
-    <Container sx={{ mt: 5 }}>
+    <Container sx={{ mt: 0, pb: 5 }}>
       <Head>
         <title>NYT Critic&#39;s Picks</title>
         <meta name="description" content="All the movies NYT Critics like" />
         <link rel="icon" href="/critics-picks-favicon.png" />
       </Head>
 
-      {/* <AppBar />
-      <Offset /> */}
+      {/* <AppBar /> */}
+      <AppBarHide />
+      {/* <Offset /> */}
 
       <UsingFetch />
     </Container>
