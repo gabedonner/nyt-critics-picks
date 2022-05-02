5b43622742ba5cbe39a6a5d60f1d45e3b438dd40
gdonner <gtdonner1@gmail.com>
2022-05-02 00:13:15 -0400

    change app bar color

--- components/app-bar.js
+++ components/app-bar.js
@@ -54,7 +54,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 export default function SearchAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
+      <AppBar position="fixed" color="secondary">
         <Toolbar>
           <Typography
             variant="h6"
